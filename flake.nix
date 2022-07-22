672e748cffffcde439bb925b9d1f2ff0645fb803
Simon Scatton <simon.scatton@epita.fr>
2022-07-22 21:07:13 +0200

    init: new django project

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Web application that converts Deezer playlists into MP3 archives.";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs , flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              python310
+              poetry
+            ];
+        };
+      }
+    );
+}
